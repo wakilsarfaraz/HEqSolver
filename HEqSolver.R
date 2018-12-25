f6c1ee562a01f2cb4a063ec21f298c3af562d97d
Wakil <wakilsarfaraz@gmail.com>
2018-12-25 17:09:51 +0000

    Solver

--- HEqSolver.R
+++ HEqSolver.R
@@ -12,31 +12,31 @@ x = matrix(rep(X,each=n),nrow=n);
 y = matrix(rep(X,m),nrow=n)
 x = c(x)
 y = c(y)
-NNODES = (N+1)^2
-U = matrix(1, NNODES, 1)
+GNodes = (N+1)^2
+U = matrix(1, GNodes, 1)
 NumTRI = 2*N^2
-LNODES = matrix(0,NumTRI,3)
+LocNodes = matrix(0,NumTRI,3)
 
 for (i in 1:N){
   for (j in 1:N){
-    LNODES[i+2*(j-1)*N,1] = i+(j-1)*(N+1)
-    LNODES[i+2*(j-1)*N,2] = i+j*(N+1)
-    LNODES[i+2*(j-1)*N,3] = (i+1)+(j-1)*(N+1)
-    LNODES[i+N+2*(j-1)*N,1] = i+1+j*(N+1)
-    LNODES[i+N+2*(j-1)*N,2] = (i+1)+(j-1)*(N+1)
-    LNODES[i+N+2*(j-1)*N,3] = i+j*(N+1)
+    LocNodes[i+2*(j-1)*N,1] = i+(j-1)*(N+1)
+    LocNodes[i+2*(j-1)*N,2] = i+j*(N+1)
+    LocNodes[i+2*(j-1)*N,3] = (i+1)+(j-1)*(N+1)
+    LocNodes[i+N+2*(j-1)*N,1] = i+1+j*(N+1)
+    LocNodes[i+N+2*(j-1)*N,2] = (i+1)+(j-1)*(N+1)
+    LocNodes[i+N+2*(j-1)*N,3] = i+j*(N+1)
   }
 }
 
-SP_Stiff <- matrix(0, NNODES, NNODES)
-SP_Mass <- matrix(0, NNODES, NNODES)
-LV = matrix(0, NNODES, 1)
+SP_Stiff <- matrix(0, GNodes, GNodes)
+SP_Mass <- matrix(0, GNodes, GNodes)
+LV = matrix(0, GNodes, 1)
 
 
 for (n in 1:NumTRI){
-  r1 = matrix(c(x[LNODES[n,1]],y[LNODES[n,1]]),nrow=2, byrow=FALSE)
-  r2 = matrix(c(x[LNODES[n,2]],y[LNODES[n,2]]),nrow=2, byrow=FALSE);
-  r3 = matrix(c(x[LNODES[n,3]],y[LNODES[n,3]]),nrow=2, byrow=FALSE);
+  r1 = matrix(c(x[LocNodes[n,1]],y[LocNodes[n,1]]),nrow=2, byrow=FALSE)
+  r2 = matrix(c(x[LocNodes[n,2]],y[LocNodes[n,2]]),nrow=2, byrow=FALSE);
+  r3 = matrix(c(x[LocNodes[n,3]],y[LocNodes[n,3]]),nrow=2, byrow=FALSE);
   J = matrix(c(r2[1]-r1[1],r2[2]-r1[2]
                ,r3[1]-r1[1],r3[2]-r1[2]), nrow=2, byrow=TRUE) 
   Astiff = (1/(2*det(J)))*matrix(c(sum((r2-r3)*(r2-r3))
@@ -48,8 +48,8 @@ for (n in 1:NumTRI){
 
   for (i in 1:3){
     for (j in 1:3){
-      SP_Stiff[LNODES[n,i],LNODES[n,j]]=SP_Stiff[LNODES[n,i],LNODES[n,j]]+Astiff[i,j]
-      SP_Mass[LNODES[n,i],LNODES[n,j]]=SP_Mass[LNODES[n,i],LNODES[n,j]]+Amass[i,j]
+      SP_Stiff[LocNodes[n,i],LocNodes[n,j]]=SP_Stiff[LocNodes[n,i],LocNodes[n,j]]+Astiff[i,j]
+      SP_Mass[LocNodes[n,i],LocNodes[n,j]]=SP_Mass[LocNodes[n,i],LocNodes[n,j]]+Amass[i,j]
     }
   }
 }
@@ -58,7 +58,7 @@ for (n in 1:NumTRI){
 TMatrix = dt * SP_Stiff + SP_Mass
 MatrixU <- matrix(0,length(T),length(U));
 
-for (i in 1:NNODES){
+for (i in 1:GNodes){
   if (x[i]==0 | y[i]==0 | x[i]==L | y[i]==L){
     TMatrix[i,] = 0
     SP_Mass[i,] = 0
@@ -76,7 +76,7 @@ for (j in 1:M+1)
 write.table(x,file="./coordx.txt",row.names=FALSE,col.names=FALSE)
 write.table(y,file="./coordy.txt",row.names=FALSE,col.names=FALSE)
 write.table(MatrixU,file="./SolutionMatrix.txt",row.names=FALSE,col.names=FALSE)
-write.table(LNODES,file="./triangles.txt",row.names=FALSE,col.names=FALSE)
+write.table(LocNodes,file="./triangles.txt",row.names=FALSE,col.names=FALSE)
 write.table(T,file="./timesteps.txt",row.names=FALSE,col.names=FALSE)
 
 
